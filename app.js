2fa11c39513a2725a046b6d306b87c6ed2e17dc1
pierce <pierceshih@gmail.com>
2019-08-10 16:38:57 +0800

    Add cors

--- app.js
+++ app.js
@@ -4,6 +4,22 @@ const bodyParser = require("body-parser");
 const app = express();
 const port = process.env.PORT || 3000;
 
+const cors = require("cors");
+
+
+// 處理跨域問題
+const origin = [
+  "http://localhost:8080",
+  "http://localhost:3000",
+  "https://ec-website-api.herokuapp.com"
+];
+const corsOptions = {
+  origin: origin,
+  credentials: true,
+  maxAge: 1728000
+};
+
+app.use(cors(corsOptions));
 app.use(bodyParser.json());
 
 app.listen(port, () => {
