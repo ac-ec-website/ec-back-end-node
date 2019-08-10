851f1713fcce0f583634a3f0964f3cfc87f5dd00
wanaka <asd8116@gmail.com>
2019-08-10 22:31:57 +0800

    Rename vscode settings

--- app.js
+++ app.js
@@ -1,31 +1,30 @@
-const express = require("express");
-const bodyParser = require("body-parser");
+const express = require('express')
+const bodyParser = require('body-parser')
 
-const app = express();
-const port = process.env.PORT || 3000;
+const app = express()
+const port = process.env.PORT || 3000
 
-const cors = require("cors");
-
+const cors = require('cors')
 
 // 處理跨域問題
 const origin = [
-  "http://localhost:8080",
-  "http://localhost:3000",
-  "https://ec-website-api.herokuapp.com"
-];
+  'http://localhost:8080',
+  'http://localhost:3000',
+  'https://ec-website-api.herokuapp.com'
+]
 const corsOptions = {
   origin: origin,
   credentials: true,
   maxAge: 1728000
-};
+}
 
-app.use(cors(corsOptions));
-app.use(bodyParser.json());
+app.use(cors(corsOptions))
+app.use(bodyParser.json())
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}!`);
-});
+  console.log(`Example app listening on port ${port}!`)
+})
 
-require("./routes")(app);
+require('./routes')(app)
 
-module.exports = app;
+module.exports = app
