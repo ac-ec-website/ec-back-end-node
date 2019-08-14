4a44226f4d425ca1e27d535ec9102850a2e1a83b
pierce <pierceshih@gmail.com>
2019-08-14 16:17:44 +0800

    Modify seeder of products, add seeder of carts and cartItems

--- seeders/20190810080218-products-seed-file.js
+++ seeders/20190810080218-products-seed-file.js
@@ -1,77 +1,106 @@
-'use strict';
+'use strict'
 
 module.exports = {
   up: (queryInterface, Sequelize) => {
-    return queryInterface.bulkInsert('Products', [{
-      name: '【草本養護】癢癢退散洗毛精-汪汪專用 500ml',
-      image: 'https://shoplineimg.com/55dbc8f4e36c8e492800000f/5cbd56f2754a4d0032420105/400x.webp?source_format=png',
-      description: "針對狗狗細緻肌膚打造溫和草本防護調理配方，舒緩皮膚敏感、幫助調節肌膚皮脂分泌。",
-      stock_quantity: 50,
-      cost_price: 250,
-      origin_price: 580,
-      sell_price: 460,
-      product_status: true,
-      createdAt: new Date(),
-      updatedAt: new Date(),
-    }, {
-      name: '【草本養護】癢癢退散洗毛精-汪汪專用 500ml',
-      image: 'https://shoplineimg.com/55dbc8f4e36c8e492800000f/5cbd56f2754a4d0032420105/400x.webp?source_format=png',
-      description: "針對狗狗細緻肌膚打造溫和草本防護調理配方，舒緩皮膚敏感、幫助調節肌膚皮脂分泌。",
-      stock_quantity: 50,
-      cost_price: 250,
-      origin_price: 580,
-      sell_price: 460,
-      product_status: true,
-      createdAt: new Date(),
-      updatedAt: new Date(),
-    }, {
-      name: '【草本養護】癢癢退散洗毛精-汪汪專用 500ml',
-      image: 'https://shoplineimg.com/55dbc8f4e36c8e492800000f/5cbd56f2754a4d0032420105/400x.webp?source_format=png',
-      description: "針對狗狗細緻肌膚打造溫和草本防護調理配方，舒緩皮膚敏感、幫助調節肌膚皮脂分泌。",
-      stock_quantity: 50,
-      cost_price: 250,
-      origin_price: 580,
-      sell_price: 460,
-      product_status: true,
-      createdAt: new Date(),
-      updatedAt: new Date(),
-    }, {
-      name: '【草本養護】癢癢退散洗毛精-汪汪專用 500ml',
-      image: 'https://shoplineimg.com/55dbc8f4e36c8e492800000f/5cbd56f2754a4d0032420105/400x.webp?source_format=png',
-      description: "針對狗狗細緻肌膚打造溫和草本防護調理配方，舒緩皮膚敏感、幫助調節肌膚皮脂分泌。",
-      stock_quantity: 50,
-      cost_price: 250,
-      origin_price: 580,
-      sell_price: 460,
-      product_status: true,
-      createdAt: new Date(),
-      updatedAt: new Date(),
-    }, {
-      name: '【草本養護】癢癢退散洗毛精-汪汪專用 500ml',
-      image: 'https://shoplineimg.com/55dbc8f4e36c8e492800000f/5cbd56f2754a4d0032420105/400x.webp?source_format=png',
-      description: "針對狗狗細緻肌膚打造溫和草本防護調理配方，舒緩皮膚敏感、幫助調節肌膚皮脂分泌。",
-      stock_quantity: 50,
-      cost_price: 250,
-      origin_price: 580,
-      sell_price: 460,
-      product_status: true,
-      createdAt: new Date(),
-      updatedAt: new Date(),
-    }, {
-      name: '【草本養護】癢癢退散洗毛精-汪汪專用 500ml',
-      image: 'https://shoplineimg.com/55dbc8f4e36c8e492800000f/5cbd56f2754a4d0032420105/400x.webp?source_format=png',
-      description: "針對狗狗細緻肌膚打造溫和草本防護調理配方，舒緩皮膚敏感、幫助調節肌膚皮脂分泌。",
-      stock_quantity: 50,
-      cost_price: 250,
-      origin_price: 580,
-      sell_price: 460,
-      product_status: true,
-      createdAt: new Date(),
-      updatedAt: new Date(),
-    }], {});
+    return queryInterface.bulkInsert(
+      'Products',
+      [
+        {
+          id: 1,
+          name: '【草本養護】癢癢退散洗毛精-汪汪專用 500ml',
+          image:
+            'https://shoplineimg.com/55dbc8f4e36c8e492800000f/5cbd56f2754a4d0032420105/400x.webp?source_format=png',
+          description:
+            '針對狗狗細緻肌膚打造溫和草本防護調理配方，舒緩皮膚敏感、幫助調節肌膚皮脂分泌。',
+          stock_quantity: 50,
+          cost_price: 250,
+          origin_price: 580,
+          sell_price: 460,
+          product_status: true,
+          createdAt: new Date(),
+          updatedAt: new Date()
+        },
+        {
+          id: 2,
+          name: '【草本養護】癢癢退散洗毛精-汪汪專用 500ml',
+          image:
+            'https://shoplineimg.com/55dbc8f4e36c8e492800000f/5cbd56f2754a4d0032420105/400x.webp?source_format=png',
+          description:
+            '針對狗狗細緻肌膚打造溫和草本防護調理配方，舒緩皮膚敏感、幫助調節肌膚皮脂分泌。',
+          stock_quantity: 50,
+          cost_price: 250,
+          origin_price: 580,
+          sell_price: 460,
+          product_status: true,
+          createdAt: new Date(),
+          updatedAt: new Date()
+        },
+        {
+          id: 3,
+          name: '【草本養護】癢癢退散洗毛精-汪汪專用 500ml',
+          image:
+            'https://shoplineimg.com/55dbc8f4e36c8e492800000f/5cbd56f2754a4d0032420105/400x.webp?source_format=png',
+          description:
+            '針對狗狗細緻肌膚打造溫和草本防護調理配方，舒緩皮膚敏感、幫助調節肌膚皮脂分泌。',
+          stock_quantity: 50,
+          cost_price: 250,
+          origin_price: 580,
+          sell_price: 460,
+          product_status: true,
+          createdAt: new Date(),
+          updatedAt: new Date()
+        },
+        {
+          id: 4,
+          name: '【草本養護】癢癢退散洗毛精-汪汪專用 500ml',
+          image:
+            'https://shoplineimg.com/55dbc8f4e36c8e492800000f/5cbd56f2754a4d0032420105/400x.webp?source_format=png',
+          description:
+            '針對狗狗細緻肌膚打造溫和草本防護調理配方，舒緩皮膚敏感、幫助調節肌膚皮脂分泌。',
+          stock_quantity: 50,
+          cost_price: 250,
+          origin_price: 580,
+          sell_price: 460,
+          product_status: true,
+          createdAt: new Date(),
+          updatedAt: new Date()
+        },
+        {
+          id: 5,
+          name: '【草本養護】癢癢退散洗毛精-汪汪專用 500ml',
+          image:
+            'https://shoplineimg.com/55dbc8f4e36c8e492800000f/5cbd56f2754a4d0032420105/400x.webp?source_format=png',
+          description:
+            '針對狗狗細緻肌膚打造溫和草本防護調理配方，舒緩皮膚敏感、幫助調節肌膚皮脂分泌。',
+          stock_quantity: 50,
+          cost_price: 250,
+          origin_price: 580,
+          sell_price: 460,
+          product_status: true,
+          createdAt: new Date(),
+          updatedAt: new Date()
+        },
+        {
+          id: 6,
+          name: '【草本養護】癢癢退散洗毛精-汪汪專用 500ml',
+          image:
+            'https://shoplineimg.com/55dbc8f4e36c8e492800000f/5cbd56f2754a4d0032420105/400x.webp?source_format=png',
+          description:
+            '針對狗狗細緻肌膚打造溫和草本防護調理配方，舒緩皮膚敏感、幫助調節肌膚皮脂分泌。',
+          stock_quantity: 50,
+          cost_price: 250,
+          origin_price: 580,
+          sell_price: 460,
+          product_status: true,
+          createdAt: new Date(),
+          updatedAt: new Date()
+        }
+      ],
+      {}
+    )
   },
 
   down: (queryInterface, Sequelize) => {
-    return queryInterface.bulkDelete('Products', null, {});
+    return queryInterface.bulkDelete('Products', null, {})
   }
-};
+}
---
+++ seeders/20190814074416-carts-my-seed-file.js
@@ -0,0 +1,26 @@
+'use strict'
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.bulkInsert(
+      'Carts',
+      [
+        {
+          id: 1,
+          createdAt: new Date(),
+          updatedAt: new Date()
+        },
+        {
+          id: 2,
+          createdAt: new Date(),
+          updatedAt: new Date()
+        }
+      ],
+      {}
+    )
+  },
+
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.bulkDelete('Carts', null, {})
+  }
+}
