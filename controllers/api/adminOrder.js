14ef8610324bc39d5f4d0e1b4d24cd4608ddbf70
wanaka <asd8116@gmail.com>
2019-08-16 23:10:19 +0800

    Add adminOrder api methods

--- controllers/api/adminOrder.js
+++ controllers/api/adminOrder.js
@@ -3,6 +3,21 @@ const Product = db.Product
 const Order = db.Order
 const OrderItem = db.OrderItem
 
-const adminOrder = {}
+const adminOrder = {
+  getOrders: async (req, res) => {
+    const orders = await Order.findAll({ include: 'items' })
+
+    return res.json({ orders })
+  },
+
+  getOrder: async (req, res) => {
+    const order = await Order.findOne({
+      where: { id: req.params.id },
+      include: 'items'
+    })
+
+    return res.json({ order })
+  }
+}
 
 module.exports = adminOrder
