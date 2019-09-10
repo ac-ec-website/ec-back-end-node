0f1deb52cf145bfe715f13d5c640bdb444ee15b7
wanaka <asd8116@gmail.com>
2019-09-11 05:47:33 +0800

    Add adProService & modify adminProduct

--- controllers/api/adminProduct.js
+++ controllers/api/adminProduct.js
@@ -1,111 +1,73 @@
 const db = require('../../models')
 const { Category, Product } = db
-
+const adProService = require('../../services/adProService')
 const imgur = require('imgur-node-api')
 const IMGUR_CLIENT_ID = process.env.IMGUR_CLIENT_ID
 
 const adminProduct = {
   getProducts: async (req, res) => {
-    const products = await Product.findAll({ include: [Category] })
+    try {
+      const { products } = await adProService.getProducts()
 
-    return res.json({ products })
+      return res.json({ products })
+    } catch (error) {
+      return res.status(422).json(error)
+    }
   },
 
   getProduct: async (req, res) => {
-    const product = await Product.findByPk(req.params.id, { include: [Category] })
+    try {
+      const productId = req.params.id
+      const { product } = await adProService.getProduct(productId)
 
-    return res.json({ product })
+      return res.json({ product })
+    } catch (error) {
+      return res.status(422).json(error)
+    }
   },
 
   postProduct: async (req, res) => {
-    if (!req.body.name) {
-      return res.json({ status: 'error', message: "name didn't exist" })
-    }
+    try {
+      if (!req.body.name) {
+        return res.json({ status: 'error', message: "name didn't exist" })
+      }
 
-    const { file } = req
-    if (file) {
-      imgur.setClientID(IMGUR_CLIENT_ID)
-      imgur.upload(file.path, async (err, img) => {
-        const productData = await Product.create({
-          name: req.body.name,
-          description: req.body.description,
-          stock_quantity: req.body.stock_quantity,
-          cost_price: req.body.cost_price,
-          origin_price: req.body.origin_price,
-          sell_price: req.body.sell_price,
-          product_status: req.body.product_status,
-          image: file ? img.data.link : null,
-          CategoryId: req.body.categoryId
-        })
-        const product = await Product.findByPk(productData.id)
-
-        return res.json({ product, status: 'success', message: 'Product was successfully created' })
-      })
-    } else {
-      const productData = await Product.create({
-        name: req.body.name,
-        description: req.body.description,
-        stock_quantity: req.body.stock_quantity,
-        cost_price: req.body.cost_price,
-        origin_price: req.body.origin_price,
-        sell_price: req.body.sell_price,
-        product_status: req.body.product_status,
-        image: null,
-        CategoryId: req.body.categoryId
-      })
-      const product = await Product.findByPk(productData.id)
+      const { file } = req
+      const data = { ...req.body }
+      const { product } = await adProService.postProduct(file, data)
 
       return res.json({ product, status: 'success', message: 'Product was successfully created' })
+    } catch (error) {
+      return res.status(422).json(error)
     }
   },
 
   putProduct: async (req, res) => {
-    if (!req.body.name) {
-      return res.json({ status: 'error', message: "name didn't exist" })
-    }
+    try {
+      if (!req.body.name) {
+        return res.json({ status: 'error', message: "name didn't exist" })
+      }
 
-    const { file } = req
-    if (file) {
-      imgur.setClientID(IMGUR_CLIENT_ID)
-      imgur.upload(file.path, async (err, img) => {
-        const product = await Product.findByPk(req.params.id)
-        await product.update({
-          name: req.body.name,
-          description: req.body.description,
-          stock_quantity: req.body.stock_quantity,
-          cost_price: req.body.cost_price,
-          origin_price: req.body.origin_price,
-          sell_price: req.body.sell_price,
-          product_status: req.body.product_status,
-          image: file ? img.data.link : product.image,
-          CategoryId: req.body.categoryId
-        })
+      const productId = req.params.id
+      const { file } = req
+      const data = { ...req.body }
+      const { product } = await adProService.putProduct(productId, file, data)
 
-        return res.json({ product, status: 'success', message: 'Product was successfully update' })
-      })
-    } else {
-      const product = await Product.findByPk(req.params.id)
-      await product.update({
-        name: req.body.name,
-        description: req.body.description,
-        stock_quantity: req.body.stock_quantity,
-        cost_price: req.body.cost_price,
-        origin_price: req.body.origin_price,
-        sell_price: req.body.sell_price,
-        product_status: req.body.product_status,
-        image: product.image,
-        CategoryId: req.body.categoryId
-      })
-
-      return res.json({ product, status: 'success', message: 'Product was successfully update' })
+      return res.json({ product, status: 'success', message: 'Product was successfully created' })
+    } catch (error) {
+      return res.status(422).json(error)
     }
   },
 
   deleteProduct: async (req, res) => {
-    const product = await Product.findByPk(req.params.id)
-    await product.destroy()
+    try {
+      const productId = req.params.id
+      await adProService.deleteProduct(productId)
 
-    return res.json({ status: 'success', message: '' })
+      return res.json({ status: 'success', message: '' })
+    } catch (error) {
+      return res.status(422).json(error)
+    }
   }
 }
 
